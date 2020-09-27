756e40b495afc8497c4b053bdb32778740b624ce
mmalaczek <miroslaw.malaczek@empik.com>
2020-09-27 19:37:09 +0200

    Skeleton for board

---
+++ src/components/Board/Board.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+    render() {
+        return (
+            <div>
+                Hello!
+            </div>
+        );
+    }
+}
+
+export default Board;
